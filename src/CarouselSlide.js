8e9b16d894b6858ea338d64c1e6b955e04bec00a
Phong Xa <etihadsep@gmail.com>
2019-10-25 19:38:05 +0700

    Initial Commit

---
+++ src/CarouselSlide.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const DefaultImg = styled.img`
+object-fit: cover;
+width: 100%;
+height: ${props =>
+    typeof props.imgHeight === 'number'
+      ? `${props.imgHeight}px`
+      : props.imgHeight};
+  `;
+
+const CarouselSlide = ({
+  Img,
+  imgUrl,
+  imgHeight,
+  description,
+  attribution,
+  ...rest }) => (
+    <figure {...rest}>
+      <Img src={imgUrl} imgHeight={imgHeight} />
+      <figcaption>
+        <strong>{description}</strong> {attribution}
+      </figcaption>
+    </figure>
+  );
+
+CarouselSlide.propTypes = {
+  Img: PropTypes.elementType,
+  imgUrl: PropTypes.string.isRequired,
+  imgHeight: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  description: PropTypes.node.isRequired,
+  attribution: PropTypes.node,
+};
+
+CarouselSlide.defaultProps = {
+  Img: DefaultImg,
+  imgHeight: 500,
+};
+
+export default CarouselSlide;
